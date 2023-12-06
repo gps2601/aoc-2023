a1a1c498bf9141797e7cc2151c1a17186d3a45b8
gps2601 <gsmith@policyexpert.co.uk>
2023-12-06 07:14:06 +0000

    day 6, part 1

---
+++ day-06/src/part1.rs
@@ -0,0 +1,66 @@
+use crate::custom_error::AocError;
+
+#[tracing::instrument]
+pub fn process() -> miette::Result<String, AocError> {
+    let race_1 = Race {
+        time: 45,
+        distance: 305,
+    };
+
+    let race_2 = Race {
+        time: 97,
+        distance: 1062,
+    };
+
+    let race_3 = Race {
+        time: 72,
+        distance: 1110,
+    };
+
+    let race_4 = Race {
+        time: 95,
+        distance: 1695,
+    };
+
+    let race_win_numbers: [i64; 4] = [race_1, race_2, race_3, race_4].map(number_of_ways_to_win);
+    let multiplied: i64 = race_win_numbers.iter().product();
+    Ok(multiplied.to_string())
+}
+
+struct Race {
+    time: i64,
+    distance: i64,
+}
+
+fn number_of_ways_to_win(race: Race) -> i64 {
+    let mut no_wins: i64 = 0;
+    for i in 0..race.time + 1 {
+        let speed = i;
+        let time_to_race = race.time - i;
+        let distance_covered = speed * time_to_race;
+        if distance_covered > race.distance {
+            no_wins += 1
+        }
+    }
+    no_wins
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let race = Race { time: 7, distance: 9 };
+        let result = number_of_ways_to_win(race);
+        assert_eq!(4, result);
+
+        let race = Race { time: 15, distance: 40 };
+        let result = number_of_ways_to_win(race);
+        assert_eq!(8, result);
+
+        let race = Race { time: 30, distance: 200 };
+        let result = number_of_ways_to_win(race);
+        assert_eq!(9, result);
+    }
+}
